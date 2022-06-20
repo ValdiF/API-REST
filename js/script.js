083ef5bb555fce55a41e2f98eed4884e676105ca
rafa <rafa_ml_smv@hotmail.com>
2022-06-20 13:09:30 +0200

    commit_rafa

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ fetch(json,options)
       //console.log(data.coches);
     let coches = data.coches
     for(let idCoches in coches){
-        allCoches.set(coches, coches[idCoches]) 
-        //console.log(allCoches.get(coches).descripcion);
+        allCoches.set(idCoches, coches[idCoches]) 
+        console.log(allCoches.get(idCoches).modelo);
     }
     cargarDatos();
     //mostrarTablaInicio();
@@ -24,11 +24,12 @@ function cargarDatos(){
     options.value = 0;
     options.innerHTML="";
     document.getElementById('cargar').appendChild(options);
-    for (coches of allCoches.keys()){
+    for (idCoches of allCoches.keys()){
+        //console.log(allCoches.get(idCoches).modelo);
         let options = document.createElement('option');
-        options.value = allCoches.get(coches).matricula;
+        options.value = allCoches.get(idCoches).matricula;
         
-        let texto = document.createTextNode(allCoches.get(coches).modelo);
+        let texto = document.createTextNode(allCoches.get(idCoches).modelo);
         options.appendChild(texto);
         document.getElementById('cargar').appendChild(options);
     }
@@ -44,6 +45,7 @@ function cargarTabla(){
     let tbody = document.createElement('tbody');
 
     let row1 = document.createElement('tr');
+    let hcolum = document.createElement('td');
     let hcolum1 = document.createElement('th');
     let hcolum2 = document.createElement('th');
     let hcolum3 = document.createElement('th');
@@ -51,6 +53,7 @@ function cargarTabla(){
     let hcolum5 = document.createElement('th');
     let hcolum6 = document.createElement('th');
 
+    hcolum.innerHTML = "IMÁGEN";
     hcolum1.innerHTML = "MATRÍCULA";
     hcolum2.innerHTML = "MODELO";
     hcolum3.innerHTML = "MARCA";
@@ -58,6 +61,7 @@ function cargarTabla(){
     hcolum5.innerHTML = "KILOMETRAJE";
     hcolum6.innerHTML = "PRECIO";
 
+    row1.appendChild(hcolum);
     row1.appendChild(hcolum1);
     row1.appendChild(hcolum2);
     row1.appendChild(hcolum3);
@@ -68,11 +72,15 @@ function cargarTabla(){
     thead.appendChild(row1);
     for (coches of allCoches.keys()){
         let comprobar = document.getElementById('cargar').selectedIndex;
+        let comp = document.getElementById('cargar').value;
+        let comprobarMatricula = allCoches.get(coches).matricula;
         if(comprobar == 0){
             document.getElementById('tabla').innerHTML="";
-            console.log(comprobar);
-        }else{
+        }else if(comprobar == comprobarMatricula){
             let row2 = document.createElement('tr');
+            let bColumn = document.createElement('td');
+            bColumn.innerHTML = "<img src="+allCoches.get(coches).img+" height='100' width='200'></img>";
+            console.log(allCoches.get(coches).img);
             let bColumn1 = document.createElement('td');
             bColumn1.innerHTML = allCoches.get(coches).matricula;
             let bColumn2 = document.createElement('td');
@@ -85,6 +93,8 @@ function cargarTabla(){
             bColumn5.innerHTML = allCoches.get(coches).kilometraje;
             let bColumn6 = document.createElement('td');
             bColumn6.innerHTML = allCoches.get(coches).precio;
+
+            row2.appendChild(bColumn);
             row2.appendChild(bColumn1);
             row2.appendChild(bColumn2);
             row2.appendChild(bColumn3);
